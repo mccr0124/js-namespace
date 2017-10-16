7c7e58891f1d1fb925b28161ea9d33d86163cb31
mccr0124 <32398053+mccr0124@users.noreply.github.com>
2017-10-16 14:30:35 -0500

    added init function

--- namespace.js
+++ namespace.js
@@ -1,1 +1,9 @@
-var MCCR0124;
+var MCCR0124 = {
+    init: function(){
+        let newDiv = document.createElement("div");
+        let box = document.getElementById("boxes");
+        newDiv.classList.add("box");
+        newDiv.textContent="mccr0124";
+        box.appendChild(newDiv);
+    }
+};
