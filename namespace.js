1b52d2021de75f150e2b476d7ff9bbcd1c2a516b
mccr0124 <32398053+mccr0124@users.noreply.github.com>
2017-10-16 14:46:21 -0500

    added onclick

--- namespace.js
+++ namespace.js
@@ -3,8 +3,16 @@ var MCCR0124 = {
         let newDiv = document.createElement("div");
         let box = document.getElementById("boxes");
         newDiv.classList.add("box");
-        newDiv.textContent="mccr0124444";
+        newDiv.textContent="mccr0124";
         box.appendChild(newDiv);
+        newDiv.addEventListener("click", divClicked);
+        newDiv.addEventListener("mouseover", divOver);
+        newDiv.addEventListener("mouseout", divOut);
         
+        function divClicked(ev){
+            newDiv.setAttribute("border-color", "blue");
+            newDiv.setAttribute("background-color", "yellow");
+        }
     }
+    
 };
